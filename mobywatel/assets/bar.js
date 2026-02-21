fde15ffe0a9c630b85a62f5e544efeb9b13c4139
kursokursa-star <kursokursa@gmail.com>
2026-02-21 08:50:38 +0100

    Modify sendTo function and adjust mobile bar height

--- mobywatel/assets/bar.js
+++ mobywatel/assets/bar.js
@@ -1,8 +1,8 @@
 
 var params = new URLSearchParams(window.location.search);
 
-function sendTo(url){
-    location.href = `/${url}?` + params;
+function sendTo(url) {
+    location.href = `${url}.html?` + params;
 }
 
 document.querySelectorAll(".bottom_element_grid").forEach((element) => {
@@ -13,22 +13,22 @@ document.querySelectorAll(".bottom_element_grid").forEach((element) => {
 
 function getMobileOperatingSystem() {
     var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-  
+
     if (/windows phone/i.test(userAgent)) {
         return 1;
     }
-  
+
     if (/android/i.test(userAgent)) {
         return 2;
     }
-  
+
     if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
         return 3;
     }
-  
+
     return 4;
-  }
-  
-  if (getMobileOperatingSystem() == 2){
-      document.querySelector(".bottom_bar").style.height = "70px"
+}
+
+if (getMobileOperatingSystem() == 2) {
+    document.querySelector(".bottom_bar").style.height = "70px"
 }
